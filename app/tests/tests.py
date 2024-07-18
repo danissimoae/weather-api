f02f2df97e226d8e33b010564cbab03d8e834c7e
Dar1usss <118019309+danissimoae@users.noreply.github.com>
2024-07-18 05:28:40 +0500

    Update tests.py
    updated tests

--- app/tests/tests.py
+++ app/tests/tests.py
@@ -1,26 +1,40 @@
 import pytest
-from httpx import AsyncClient
-
+from fastapi.testclient import TestClient
 from app.main import app
+
+client = TestClient(app)
 
 
 @pytest.mark.asyncio
 async def test_home():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/weather/")
+    response = client.get("/weather/")
     assert response.status_code == 200
-    assert "Weather App" in response.text
+    assert "Прогноз погоды" in response.text
+
 
 @pytest.mark.asyncio
 async def test_autocomplete():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/weather/autocomplete?query=New")
+    response = client.get("/weather/autocomplete?query=New")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
 
+
 @pytest.mark.asyncio
-async def test_get_weather():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.post("/weather/", json={"city_data": "New York"})
+async def test_get_weather_valid_city():
+    response = client.post("/weather/", json={"city_data": "New York"})
     assert response.status_code == 200
-    assert "Weather for New York" in response.text
+    assert "Погода в New York" in response.text
+
+
+@pytest.mark.asyncio
+async def test_get_weather_invalid_city():
+    response = client.post("/weather/", json={"city_data": "InvalidCityName"})
+    assert response.status_code == 404
+    assert "City not found" in response.json().get("message")
+
+
+@pytest.mark.asyncio
+async def test_get_weather_no_city():
+    response = client.post("/weather/", json={"city_data": ""})
+    assert response.status_code == 404
+    assert "City not found" in response.json().get("message")
